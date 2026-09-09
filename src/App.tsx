e310b09176a9d262af6275aed9e52132fa60ef1f
balb3z <balbaa090@gmail.com>
2026-09-09 05:24:13 +0300

    react import

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ProtectedRoute } from '@/components/auth/ProtectedRoute';
